c3e08e56fd88332cae45afa39aff35eb805c8f06
Mohammed Salam <mohammed.salam912@myhunter.cuny.edu>
2019-10-10 15:16:21 -0400

    Api returns card info in deck

--- Gwent/Gwent/ApiControllers/DeckApiController.cs
+++ Gwent/Gwent/ApiControllers/DeckApiController.cs
@@ -1,5 +1,6 @@
 ﻿using Gwent.Models;
 using Gwent.Repositories;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Http;
@@ -22,11 +23,28 @@ namespace Gwent.ApiControllers
         async public Task<ShortDeckInfo> Post()
         {
             Deck deck = await _repository.CreateNewShuffledDeckAsync();
+
+            List<ShortCardInfo> cardInfos = new List<ShortCardInfo>();
+
+            foreach (var card in deck.Cards)
+            {
+                var shortCardInfo = new ShortCardInfo
+                {
+                    Name = card.Name,
+                    Description = card.Description,
+                    Strength = card.Strength.Value,
+                    CardType = card.CardType,
+                    SpecialAbility = card.SpecialAbility
+                };
+                cardInfos.Add(shortCardInfo);
+            }
+
             ShortDeckInfo deckInfo = new ShortDeckInfo
             {
                 DeckId = deck.Id,
                 Faction = deck.Faction,
-                Remaining = deck.Cards.Where(d => !d.Drawn).Count()
+                Remaining = deck.Cards.Where(d => !d.Drawn).Count(),
+                Cards = cardInfos
             };
 
             return deckInfo;
--- Gwent/Gwent/Models/ShortDeckInfo.cs
+++ Gwent/Gwent/Models/ShortDeckInfo.cs
@@ -1,9 +1,12 @@
-﻿namespace Gwent.Models
+﻿using System.Collections.Generic;
+
+namespace Gwent.Models
 {
     public class ShortDeckInfo
     {
         public int DeckId { get; set; }
         public int Remaining { get; set; }
         public string Faction { get; set; }
+        public List<ShortCardInfo> Cards { get; set; }
     }
 }
