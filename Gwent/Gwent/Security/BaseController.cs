dda7e2ed0dde8775e203616c904949097a740eca
Omer Latif <olatif@u.rochester.edu>
2019-10-10 10:33:04 -0400

    commit right before adding Nuget Package for BCrypt

---
+++ Gwent/Gwent/Security/BaseController.cs
@@ -0,0 +1,39 @@
+﻿using Gwent.Data;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gwent.Security
+{
+    public class BaseController : Controller
+    {
+        protected Context context;
+        public BaseController()
+        {
+            context = new Context();
+
+            context.Database.Log = (message) => Debug.WriteLine(message);
+        }
+
+        private bool disposed = false;
+        protected override void Dispose(bool disposing)
+        {
+            if (disposed == true)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                context.Dispose();
+            }
+
+            disposed = true;
+
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Gwent/Gwent/Security/CustomPrincipal.cs
@@ -0,0 +1,41 @@
+﻿using Gwent.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+
+namespace Gwent.Security
+{
+    public class CustomPrincipal :IPrincipal
+    {
+        private CustomIdentity identity;
+        private User user;
+
+        public CustomPrincipal(CustomIdentity identity, User user)
+        {
+            this.identity = identity;
+            this.user = user;
+        }
+
+        public IIdentity Identity
+        {
+            get
+            {
+                return identity;
+            }
+        }
+        public User User
+        {
+            get
+            {
+                return user;
+            }
+        }
+
+        public bool IsInRole(string role)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
