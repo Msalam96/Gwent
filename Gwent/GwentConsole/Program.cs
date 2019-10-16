1de91ab9b1a6ea4370f2f4079727d9f0fc8c238e
Omer Latif <olatif@u.rochester.edu>
2019-10-16 11:31:35 -0400

    started creating classes

--- Gwent/GwentConsole/Program.cs
+++ Gwent/GwentConsole/Program.cs
@@ -22,6 +22,24 @@ namespace GwentConsole
                 test.StartNewGame(1, 2);
                 Console.ReadKey();
             }
+
+
+            //GameApiController used to get begining state of the game
+            //StartNewGame is a POST method where we create the Pile and Create a new game
+            //This POST returns PlayerOneId and PlayerTwoId
+            //Start the first round
+            //Clicking a card generates a post
+            //This POST also changes activePlayerId in GameRound
+            //Changes the location of the PileCard and adds to GameRoundCard
+            //The clicked card has a pilecardId
+            //ActivePlayerId column in GameRound keeps track of who's the current player
+            //Pass
+
+            //GameLogic class with 3-4 methods, one being StartGame()
+            //PlayCard()
+            //
+            //GetGameState(int GameId)
+            //
         }
     }
 }
