8c996a6e7f01f43fb0964fb49d158afce8ed97e9
Sarthak Thakur <sarthakthakur0097@gmail.com>
2019-10-17 11:15:16 -0400

    cleanup

--- Gwent/Gwent/Scripts/notificationCount.js
+++ Gwent/Gwent/Scripts/notificationCount.js
@@ -1,18 +1,26 @@
 ﻿(async() =>{
     const gebi = (e) => document.getElementById(e);
-    const notificationLink = gebi("notificationLink");
+    const countSpan = gebi("totalNotifications");
 
-    let notificationCount = localStorage.getItem("notifiCount");
-    myStorage = window.localStorage;
+    const notificationCountString = localStorage.getItem("notifiCount");
+    let notificationCount = parseInt(notificationCountString, 10);
+    if (isNaN(notificationCount)) {
+        notificationCount = 0;
+    }
+
     let lastMessageTime = new Date();
     setInterval(async () => {
         const nextMessageTime =  new Date();
         console.log("Retrieving messages from: "+lastMessageTime);
-        await getNotifications(lastMessageTime);
+        const notifications = await getNotifications(lastMessageTime);
         lastMessageTime = nextMessageTime;
-        console.log("To Display " + notificationCount);
-        countSpan.innerHTML = notificationCount;
-          
+
+        notificationCount += notifications.length;
+        localStorage.setItem("notifiCount", notificationCount);
+
+        console.log("STORED ITEM IS : " + localStorage.getItem("notifiCount"))
+
+        countSpan.innerHTML = notificationCount;          
     }, 1000);
 
     async function getNotifications(from)
@@ -21,25 +29,7 @@
         const response = await fetch(url, {
             credentials:"include",
         });
-        const data = await response.json();
-        
-        countSpan = gebi("totalNotifications");
-        totCount = getCount(data);
-        localStorage.setItem("notifiCount", totCount)
-
-        console.log("STORED ITEM IS : " + localStorage.getItem("notifiCount"))
+        const data = await response.json();        
+        return data;
     }
-    debugger;
-    //notificationLink.addEventListener('click', async (event) => {
-    //    localStorage.setItem("notifiCount", 0)
-    //});
-   
-    function getCount(notifications) {
-        if(notifications.length===0)
-        {
-            return notificationCount;
-        }
-            return notificationCount++;
-    };
-   
 })();
